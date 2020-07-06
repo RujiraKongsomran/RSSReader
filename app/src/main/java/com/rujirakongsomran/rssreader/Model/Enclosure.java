7c69b7cdfdc00300bb4ddb50f2f9342b7f95293c
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-07-06 07:48:38 +0700

    Create Model for RSS

---
+++ app/src/main/java/com/rujirakongsomran/rssreader/Model/Enclosure.java
@@ -0,0 +1,17 @@
+package com.rujirakongsomran.rssreader.Model;
+
+public class Enclosure {
+    public String link;
+
+    public Enclosure(String link) {
+        this.link = link;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
